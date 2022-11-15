e0b627cc4a3942351748ce3fe09f7c11cfada80d
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-11-16 01:36:47 +0300

    modified brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
+	userName = String;
 	const userName = readlineSync.question('May I have your name?');
-	return 'Hello,' + userName + '!';
-};
-
+	console.log("Welcome to the Brain Games!");
+	console.log('Hello,' + userName + '!');	
+};	
