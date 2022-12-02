53a1a5201cd893b233b073fb3bdab6315a0290f9
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-02 09:38:24 +0300

    modified calc.js cli.js even.js

--- src/calc.js
+++ src/calc.js
@@ -1,38 +1,37 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export const greeting = () => {
-	        console.log('Welkome to the Brain Games!')
+  console.log('Welkome to the Brain Games!')
 }
 
-
 const random = (max) => {
-	        return Math.floor(Math.random() * max);
-	        };
+  return Math.floor(Math.random() * max)
+}
 
-const operatorArray = ['+', '-', '*']
-	const index = Math.floor(Math.random()*operatorArray.length);
-	const operator = operatorArray[index];
-	return Number(operator);
+const getRandomOperator = () => {
+  const operatorArray = ['+', '-', '*']
+  const index = Math.floor(Math.random() * operatorArray.length)
+  let operator = operatorArray[index]
+  return operator
+}
 
 export const brainCalc = () => {
-	        const userName = readlineSync.question('May I have your name?  ');
-	                console.log('Hello, ' + userName + '!');
-
-	        console.log('What is the result of the expression?');
-
-	        for (let i = 0; i < 3; i++) {
-			const number1 = random(10);
-			const number2 = random(10);
-			const result = `${number1} ${Number(operator)} ${number2}`;
-			console.log('Question: ' + number1 + Number(operator) + number2);
-			const answer = readlineSync.question('Your answer: ');
-			if (answer = result)  {
-				console.log('Correct!')
-				} else {
-					const result = (answer + " is wrong answer ;(. Correct answer was" + result + ". \n Let's try again, " + userName + "!");
-					console.log(result);
-					break;
-					};
-			                console.log('Congratulations, ' + userName + '!');
-			                }
-	        };
+  const userName = readlineSync.question('May I have your name?  ')
+  console.log('Hello, ' + userName + '!')
+  console.log('What is the result of the expression?')
+  for (let i = 0; i < 3; i++) {
+    const number1 = random(10)
+    const number2 = random(10)
+    const result = `${number1} ${Number(operator)} ${number2}`
+    console.log('Question: ' + number1 + Number(operator) + number2)
+    const answer = readlineSync.question('Your answer: ')
+    if (answer === result) {
+      console.log('Correct!')
+    } else {
+      const result = `${answer} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`
+      console.log(result)
+      break
+    }
+    console.log('Congratulations, ' + userName + '!')
+  }
+}
--- src/even.js
+++ src/even.js
@@ -1,33 +1,29 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export const greeting = () => {
-	console.log('Welkome to the Brain Games!')
+  console.log('Welkome to the Brain Games!')
 }
 
-
 const random = (max) => {
-	return Math.floor(Math.random() * max);
-	};
+  return Math.floor(Math.random() * max)
+}
 
 export const brainEven = () => {
-	const userName = readlineSync.question('May I have your name?  ');
-	        console.log('Hello, ' + userName + '!');
-	        
-	console.log('Answer "yes" if the number is even, otherwise answer "no".');
-	
-	for (let i = 0; i < 3; i++) {
-		const number = random(100);
-		console.log('Question: ' + number);
-		const answer = readlineSync.question('Your answer: ');
-		if (number % 2 == 0 && answer == 'yes' || number % 2 != 0 && answer == 'no') {
-			console.log('Correct!')
-			} else {
-				const incorrect = (answer == 'yes' ? 'no': 'yes');
-				
-				const result = (answer + " is wrong answer ;(. Correct answer was" + incorrect + ". \n Let's try again, " + userName + "!");
-				console.log(result);
-				break;
-				};
-		console.log('Congratulations, ' + userName + '!');
-		}
-	};
+  const userName = readlineSync.question('May I have your name?  ')
+  console.log('Hello, ' + userName + '!')
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  for (let i = 0; i < 3; i++) {
+    const number = random(100)
+    console.log('Question: ' + number)
+    const answer = readlineSync.question('Your answer: ')
+    if (((number % 2 === 0) && (answer === 'yes')) || ((number % 2 !== 0) && (answer === 'no'))) {
+      console.log('Correct!')
+    } else {
+      const incorrect = (answer === 'yes' ? 'no' : 'yes')
+      const result = (answer + ' is wrong answer ;(. Correct answer was' + incorrect + '. \n Lets try again, ' + userName + '!')
+      console.log(result)
+      break
+    }
+    console.log('Congratulations, ' + userName + '!')
+  }
+}
