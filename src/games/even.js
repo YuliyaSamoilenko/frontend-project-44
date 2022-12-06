232e1a3bef7e3e6b7fa20c6327c9926d2d455449
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-06 16:19:12 +0300

    modified index and even

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,13 @@
-import readlineSync from 'readline-sync'
+import {game} from "./index.js"
 
-import {random, name} from "./index.js"
+import {random} from "./index.js"
 
-export const brainEven = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".')
-  for (let i = 0; i < 3; i++) {
-    const number = random(100)
-    console.log('Question: ' + number)
-    const answer = readlineSync.question('Your answer: ')
+const quest = 'Answer "yes" if the number is even, otherwise answer "no".'
+
+const getQuestion = random(100)
     if (((number % 2 === 0) && (answer === 'yes')) || ((number % 2 !== 0) && (answer === 'no'))) {
-      console.log('Correct!')
+      const answer = correct
     } else {
       const correct = (answer === 'yes' ? 'no' : 'yes')
-      const result = `${answer} is wrong answer ;(. Correct answer was ${correct}.\nLet's try again, ${name()}!`
-      console.log(result)
-      break
     }
-    console.log(`Congratulations, ${name()}!`)
-  }
-}
+
--- src/games/index.js
+++ src/games/index.js
@@ -4,12 +4,24 @@ export const greeting = () => {
     console.log('Welkome to the Brain Games!')
 }
 
-export const name = () => {
-const userName = readlineSync.question('May I have your name?  ')
-    console.log('Hello, ' + userName + '!')
-    return userName
-}
-
 export const random = (max) => {
     return Math.floor(Math.random() * max)
   }
+
+export const game = () => {
+    const userName = readlineSync.question('May I have your name?  ')
+    console.log('Hello, ' + userName + '!')
+    console.log(`${quest}`)
+    for (i = 0; i < 3; i++) {
+    const question = getQuestion
+    console.log(`Question: ${question}`)
+    const answer = readlineSync.question('Your answer: ')
+    if (answer === correct) {
+        console.log('Correct!')
+    } else {
+        console.log(`${answer} is wrong answer ;(. Correct answer was ${correct}.\nLet's try again, ${userName}!`)
+        break
+    }
+}
+console.log(`Congratulations, ${userName}!`)
+}
