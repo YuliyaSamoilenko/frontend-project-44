b8a16b0c2cb5e0ca2a83658f738c21803743d421
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-12 13:50:16 +0300

    modifid cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync'
 const name = () => {
   const userName = readlineSync.question('May I have your name?')
   console.log('Welcome to the Brain Games!')
-  console.log('Hello,' + userName + '!')
+  console.log('Hello, ' + userName + '!')
 }
 export default name
