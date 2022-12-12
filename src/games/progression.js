f8b5a5c9cf51cd5c9e05c92b376be0d8a49a6229
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-12 15:33:07 +0300

    progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,6 +1,6 @@
 import getGame from "../index.js";
 
-import { randomNumber } from "../index.js";
+import {randomNumber} from "../index.js";
 
 const taskGame = 'What number is missing in the progression?'
 
@@ -8,16 +8,15 @@ const arithmProgression = (number1, quantityNumber, step) => {
     
     const arithmProgressionArray = []
     for (let i = 0; i <= quantityNumber; i ++) {
-        const number = number1 + step * i
+        const number = number1 + (step * i)
         arithmProgressionArray.push(number)
-        
     }
     return arithmProgressionArray
 }
 
 const roundGame = () => {
     const number1 = Math.abs(randomNumber(10))
-    const step = Math.abs(randomNumber(10))
+    const step = Math.abs(randomNumber(9) + 1)
     const quantityNumber = Math.abs(randomNumber(5) + 5)
     const unkownMemberIndex = Math.abs(randomNumber(5) + 4)
    
