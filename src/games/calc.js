88fd382c4d86039273d05804a2224c120f74b4b0
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-13 19:52:40 +0300

    calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -9,13 +9,17 @@ const getRandomOperator = () => {
 };
 
 const calculation = (number1, number2, operator) => {
+  let result = 0;
   switch (operator) {
     case '+':
-      return number1 + number2;
+      result = number1 + number2;
+      return result;
     case '-':
-      return number1 - number2;
+      result = number1 - number2;
+      return result;
     case '*':
-      return number1 * number2;
+      result = number1 * number2;
+      return result;
   }
 };
 
