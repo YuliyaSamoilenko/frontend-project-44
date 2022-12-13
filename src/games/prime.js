13738c0778a554ee96bae7c640a5eb120d358399
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-13 23:48:24 +0300

    prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,9 @@ import getGame, { getRandomNumber } from '../index.js';
 const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const calculation = (number) => {
+  if (number <=1) {
+    return false;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return 'no';
