72adca3a0ddfc3870db8fc6955567a903347d762
YuliyaSamoilenko <yuliyashks@gmail.com>
2022-12-13 17:04:55 +0300

    modified files

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,38 +1,37 @@
-import getGame from "../index.js"
+import getGame from '../index.js';
+import { randomNumber } from '../index.js';
 
-import {randomNumber} from "../index.js"
-
-const taskGame = 'What is the result of the expression?'
+const taskGame = 'What is the result of the expression?';
              
 const getRandomOperator = () => {
-    const operatorArray = ['+', '-', '*']
-    const index = Math.floor(Math.random() * operatorArray.length)
-    let selectedOperator = operatorArray[index]
-    return selectedOperator
-  }
+    const operatorArray = ['+', '-', '*'];
+    const index = Math.floor(Math.random() * operatorArray.length);
+    let selectedOperator = operatorArray[index];
+    return selectedOperator;
+  };
 
 const calculation = (number1, number2, operator) => {
   switch (operator) {
     case "+":
-      return number1 + number2
+      return number1 + number2;
         case "-":
         return number1 - number2;
           case "*":
           return number1 * number2;
-  }
-}
+  };
+};
 
 const roundGame = () => {
-  const number1 = randomNumber(10)
-  const number2 = randomNumber(10)
-  const operator = getRandomOperator()
-  const question = `${number1} ${operator} ${number2}`
-  const answer = calculation(number1, number2, operator)
-  return [question, answer]  
-}
+  const number1 = randomNumber(10);
+  const number2 = randomNumber(10);
+  const operator = getRandomOperator();
+  const question = `${number1} ${operator} ${number2}`;
+  const answer = calculation(number1, number2, operator);
+  return [question, answer];
+};
 
 export default () => {
-  getGame(taskGame, roundGame)
-}
+  getGame(taskGame, roundGame);
+};
     
 
--- src/games/even.js
+++ src/games/even.js
@@ -1,17 +1,18 @@
-import getGame from "../index.js"
+import getGame from '../index.js';
 
-import {randomNumber} from "../index.js"
+import {randomNumber} from '../index.js';
 
-const even = (number) => number % 2 === 0
-const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".'
+const even = (number) => number % 2 === 0;
+
+const taskGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const roundGame = () => {
-  const number = randomNumber(100)
-  const question = `${number}`
-  const answer = even(number) ? 'yes' : 'no'
-  return [question, answer]
-}
+  const number = randomNumber(100);
+  const question = `${number}`;
+  const answer = even(number) ? 'yes' : 'no';
+  return [question, answer];
+};
 
 export default () => {
-  getGame(taskGame, roundGame) 
-}
+  getGame(taskGame, roundGame);
+};
